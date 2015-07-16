305fd432b969f55ecafb825e644d7f7c1f47a132
Liam Da Silva <liam_dasilva@yahoo.com>
2015-07-16 06:51:13 -0400

    added new post functionality and signup functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,14 +15,16 @@ angular.module('app.controllers', [])
   $scope.loginData.password="";
 
   // Form data for the login modal
-  $scope.signupData = {};
-  $scope.signupData.username="";
-  $scope.signupData.email="";
-  $scope.signupData.firstName="";
-  $scope.signupData.lastName="";
-  $scope.signupData.password="";
-  $scope.signupData.passwordc="";
-
+  $scope.signUpData = {};
+  $scope.signUpData.username="";
+  $scope.signUpData.email="";
+  $scope.signUpData.firstName="";
+  $scope.signUpData.lastName="";
+  $scope.signUpData.password="";
+  $scope.signUpData.passwordc="";
+  $scope.signUpErrorMessage = "";
+  $scope.error = false;
+  $scope.incomplete = true;  
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -53,16 +55,16 @@ angular.module('app.controllers', [])
     $scope.signupModal.hide();
   };
   // Open the login modal
-  $scope.signup = function() {
+  $scope.signup = function() {  
     $scope.signupModal.show();
   };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    console.log('Doing login');
     Parse.User.logIn($scope.loginData.username, $scope.loginData.password,{
       success: function(user){
-        var name = user.get("firstName");
+        $scope.name = user.get("firstName");
         alert("Hello " + name + ", you are now logged in.");
         $scope.closeLogin();
       },
@@ -77,39 +79,148 @@ angular.module('app.controllers', [])
     Parse.User.logOut();
     alert("You are now logged out");
   };
+  $scope.$watch('signUpData.username',function() {$scope.test();});
+  $scope.$watch('signUpData.email',function() {$scope.test();});
+  $scope.$watch('signUpData.firstName', function() {$scope.test();});
+  $scope.$watch('signUpData.lastName', function() {$scope.test();});
+  $scope.$watch('signUpData.password', function() {$scope.test();});
+  $scope.$watch('signUpData.passwordc', function() {$scope.test();});
 
-  $scope.doSignup = function() {
-    console.log('Doing signup', $scope.loginData);
-    Parse.User.logIn($scope.loginData.username, $scope.loginData.password,{
-      success: function(user){
-        var name = user.get("firstName");
-        alert("Hello " + name + ", you are now logged in.");
+  $scope.test = function() {
+    if ($scope.signUpData.password !== $scope.signUpData.passwordc) {
+      $scope.error = true;
+    } else {
+      $scope.error = false;
+    }
+    $scope.incomplete = false;
+    if (!$scope.signUpData.username.length ||
+      !$scope.signUpData.lastName.length ||
+      !$scope.signUpData.firstName.length || 
+      !$scope.signUpData.password.length || 
+      !$scope.signUpData.email.length) {
+          $scope.incomplete = true;
+    }
+  };
+
+  $scope.doSignUp = function() {
+    console.log('Doing signup');
+    signUpNewUser($scope.signUpData).then(
+      function(result){
+        alert("You successfully signed up!");
+        $scope.closeSignup();
+        $scope.signUpData.username="";
+        $scope.signUpData.email="";
+        $scope.signUpData.firstName="";
+        $scope.signUpData.lastName="";
+        $scope.signUpData.password="";
+        $scope.signUpData.passwordc="";
+        $scope.signUpErrorMessage = "";
+        $scope.error = false;
+        $scope.incomplete = true;
         $scope.closeLogin();
-      },
-      error:function(user,error){
-        alert(error.message);
-      }
+    },function(error){
+      $scope.signUpErrorMessage = error;
+      $scope.signUpError = true;
     });
   };
 }])
 
 .controller('DashboardCtrl', ['$scope',function($scope) {
-  $scope.buying = [];
-  $scope.selling = [];
   var user = Parse.User.current();
-  $scope.buying = getPostings("Buyer",user);
-  $scope.selling = getPostings("Seller",user);
-  $scope.noBuyPostings = $scope.buying == [];
-  $scope.noSellPostings = $scope.selling == [];
+  getPostings("Buyer",user).then(function(result){
+    $scope.buying = result;
+    $scope.noBuyPostings = $scope.buying.length == 0;
+  }, function(error){
+    alert(error);
+  });
+  getPostings("Seller",user).then(function(result){
+    $scope.selling = result;
+    $scope.noSellPostings = $scope.selling.length == 0;
+  }, function(error){
+    alert(error);
+  });
 
 }])
-.controller('NewBuyPostingCtrl', ['$scope',function($scope) {
+.controller('NewBuyPostingCtrl', ['$scope', '$state',function($scope,$state) {
+  $scope.posting = {};
+  $scope.posting.courseCode = "";
+  $scope.posting.price = "";
+  $scope.posting.edition = "";
+  $scope.posting.tName = "";
 
+  $scope.save = function(){
+    saveBuyPosting($scope.posting,"Buyer", false).then(
+      function(result){
+      alert("Save successful");
+      $scope.posting.courseCode = "";
+      $scope.posting.price = "";
+      $scope.posting.edition = "";
+      $scope.posting.tName = "";
+      $state.go('app.dashboard')
+
+    }, function(error){
+      alert(error);
+    });
+  }
+
+  $scope.post = function(){
+    //save buy posting with visibility as true
+    saveBuyPosting($scope.posting,"Buyer", true).then(
+      function(result){
+      alert("Post successful");
+      $scope.posting.courseCode = "";
+      $scope.posting.price = "";
+      $scope.posting.edition = "";
+      $scope.posting.tName = "";
+      $state.go('app.dashboard')
+
+    }, function(error){
+      alert(error);
+    });
+  }
 }])
-.controller('NewSellPostingCtrl', ['$scope',function($scope) {
+.controller('NewSellPostingCtrl', ['$scope','$state',function($scope,$state) {
+  $scope.posting = {};
+  $scope.posting.courseCode = "";
+  $scope.posting.price = "";
+  $scope.posting.edition = "";
+  $scope.posting.tName = "";
+  $scope.posting.condition = "Good";
 
+  $scope.save = function(){
+    savePosting($scope.posting,"Seller", false).then(
+      function(result){
+      alert("Save successful");
+      $scope.posting.courseCode = "";
+      $scope.posting.price = "";
+      $scope.posting.edition = "";
+      $scope.posting.tName = "";
+      $state.go('app.dashboard')
+
+    }, function(error){
+      alert(error);
+    });
+  }
+
+  $scope.post = function(){
+    //save buy posting with visibility as true
+    savePosting($scope.posting,"Seller", true).then(
+      function(result){
+      alert("Post successful");
+      $scope.posting.courseCode = "";
+      $scope.posting.price = "";
+      $scope.posting.edition = "";
+      $scope.posting.tName = "";
+      $state.go('app.dashboard')
+
+    }, function(error){
+      alert(error);
+    });
+  }
 }])
 .controller('SingleBuyEntryCtrl', ['$scope', '$stateParams',function($scope, $stateParams) {
+  $scope.buyId = $stateParams.objectId;
 }])
 .controller('SingleSellEntryCtrl', ['$scope', '$stateParams',function($scope, $stateParams) {
+  $scope.sellId = $stateParams.objectId;
 }]);
--- www/js/functions.js
+++ www/js/functions.js
@@ -2,20 +2,110 @@ var getPostings = function(tableName,user){
   var query = new Parse.Query(tableName);
   query.equalTo("author",user);
   var postings = [];
-  query.find({
-    success: function(results) {
+  //set up a promise to return
+  var promise = new Parse.Promise();
+  query.find().then(function(results) {
       //alert("Successfully retrieved " + results.length + " buy posts.");
       // Do something with the returned Parse.Object values
       for (var i = 0; i < results.length; i++) {
         var object = results[i].toJSON();
         postings.push(object);
-        //alert(object.objectId + ' - ' + object.title);
       }
+      //fires the .then() in the code calling this function
+      promise.resolve(postings);
+    },function(error){
+      //fires the error part in the .then() in the code calling this function
+      promise.reject("Error: " + error.message);
+    });
+  return promise;
+}
+
+//signs up a new user and logs them in given a userData object
+//the userData object contains username, email, password, firstName and lastName
+var signUpNewUser = function(userData){
+  var user = new Parse.User();
+  user.set("username", userData.username);
+  user.set("password", userData.password);
+  user.set("email", userData.email);
+  user.set("firstName", userData.firstName);
+  user.set("lastName", userData.lastName);
+  var promise = new Parse.Promise();
+  user.signUp(null, {
+    success: function(user) {
+        promise.resolve("Sign Up successful");
     },
-    error: function(error) {
-      alert("Error: " + error.code + " " + error.message);
+    error: function(user, error) {
+      // Show the error message somewhere and let the user try again.
+        promise.reject("Error: " + error.message);
     }
   });
-  return postings;
+  return promise;
 }
-  
+
+var savePosting = function(postData, tableName, visible){
+  //set up a promise to return
+  var promise = new Parse.Promise();
+  if (tableName == "Buyer" && postData.courseCode != "" && postData.tName != "" && postData.price != ""){
+    var Posting = Parse.Object.extend(tableName);
+    var posting = new Posting();
+
+    if (postData.edition != ""){
+      posting.set("edition", Number(postData.edition));
+    }
+    posting.set("author",Parse.User.current());
+    posting.set("courseCode", postData.courseCode);
+    posting.set("title", postData.tName);
+    posting.set("price", Number(postData.price));
+    posting.set("visibility", visible);//a saved post can only be seen by the user
+    if(!visible){
+      posting.setACL(new Parse.ACL(Parse.User.current()));
+    }else{
+      var myACL = new Parse.ACL(Parse.User.current());
+      myACL.setPublicReadAccess(true);
+      posting.setACL(myACL);
+    }
+    
+    posting.save(null, {
+      success: function(user) {
+          promise.resolve("Save successful");
+      },
+      error: function(user, error) {
+        // Show the error message somewhere and let the user try again.
+          promise.reject("Error: " + error.message);
+      }
+    });
+  } else if (tableName == "Seller" && postData.courseCode != "" && postData.tName != "" && postData.price != "" && postData.condition != "" && postData.edition != ""){
+    var Posting = Parse.Object.extend(tableName);
+    var posting = new Posting();
+
+    if (postData.edition != ""){
+      posting.set("edition", Number(postData.edition));
+    }
+    posting.set("author",Parse.User.current());
+    posting.set("courseCode", postData.courseCode);
+    posting.set("title", postData.tName);
+    posting.set("price", Number(postData.price));
+    posting.set("condition", postData.condition);
+    posting.set("visibility", visible);//a saved post can only be seen by the user
+    if(!visible){
+      posting.setACL(new Parse.ACL(Parse.User.current()));
+    }else{
+      var myACL = new Parse.ACL(Parse.User.current());
+      myACL.setPublicReadAccess(true);
+      posting.setACL(myACL);
+    }
+    
+    posting.save(null, {
+      success: function(user) {
+          promise.resolve("Save successful");
+      },
+      error: function(user, error) {
+        // Show the error message somewhere and let the user try again.
+          promise.reject("Error: " + error.message);
+      }
+    });
+  }else{
+    promise.reject("Please leave nothing empty!");
+  }
+  return promise;
+}
